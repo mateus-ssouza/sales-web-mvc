820d22e320ac9644e9049ccc6885dbfaa862898c
Mateus Souza <msouzacrv@gmail.com>
2023-06-12 08:26:47 -0300

    Model-Controller-View: Departamento

---
+++ SalesWebMvc/Controllers/DepartamentosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento
+            {
+                Id = 1,
+                Nome = "Eletronics"
+            });
+            
+            list.Add(new Departamento
+            {
+                Id = 1,
+                Nome = "Fashion"
+            });
+
+            return View(list);
+        }
+    }
+}
